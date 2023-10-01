fd8bb4bc4c2ead032f06fd5c465082c25f9fbabb
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-10-02 00:14:07 +0530

    with-pointer

--- cpp/structure.cpp
+++ cpp/structure.cpp
@@ -11,6 +11,11 @@ int main()
     student s1;
     s1.name="himanshu singh";
     s1.roll_no=03;
-    cout<<s1.name<<" "<<s1.roll_no;
+    cout<<s1.name<<" "<<s1.roll_no<<endl;
+    // also we can access the element of the structure with the help of pointer 
+    struct student *s;
+    s=&s1;
+    s->name="rahul singh";
+    cout<<s->name;
 
 }
