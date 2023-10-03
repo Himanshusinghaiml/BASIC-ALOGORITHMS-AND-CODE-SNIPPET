70c9d7585fb687999d3a3d8f57abcc6b9fd33572
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-10-03 21:03:26 +0530

    binaryalgorithm

---
+++ cpp/binary_search.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binaryrecursive(vector<int>arr,int target,int i,int j)
+{ 
+    if(i<=j)
+    {
+
+    int mid=i+(j-i)/2;
+    if(arr[mid]==target)return target;
+    if(arr[mid]<target)return binaryrecursive(arr,target,mid+1,j);
+    else return binaryrecursive(arr,target,i,mid-1);
+
+    }
+    return -1;
+    
+}
+int binary(vector<int>arr,int target)
+{   // iterative method 
+     int i=0;int j=arr.size()-1;
+     while(i<=j)
+     {
+        int mid=i+(j-i)/2;
+        if(arr[mid]==target)return target;
+        else if(arr[mid]<target) i=mid+1;
+        else j=mid-1;
+     }   
+     return -1;
+}
+int main()
+{
+    vector<int>arr;
+    for(int i=1;i<=10;i++)
+    {
+        arr.push_back(i*10);
+    }
+    
+    for(auto i:arr)cout<<i<<" ";// for printing the element of the vector
+    int target=30;
+    // iterative method function
+    cout<<endl<<" iterative ans"<<endl;
+    int ans=binary(arr,target);
+    if(ans!=-1)cout<<endl<<"element found :  "<<ans<<endl;else cout<<endl<<"not found"<<endl;
+
+    // rerursive call  binary search
+    cout<<"recursive ans"<<endl;
+    int i=0;int j=arr.size()-1;
+    int binaryans=binaryrecursive(arr,target,i,j);
+    if(binaryans!=-1)cout<<" element  found : "<<binaryans<<endl;else cout<<" element  not found : " ;
+     
+    return 0;
+}
--- cpp/structure.cpp
+++ cpp/structure.cpp
@@ -6,17 +6,24 @@ struct  student
     int roll_no;
     string  name;
 };
-
+void callingchar()
+{
+    int n=65;
+    char ans=char(n);
+    cout<<ans<<endl;
+    cout<<n<<" "<<char(n)<<endl<< string(1,char(n));
+}
 int main()
 {
     student s1;
     s1.name="himanshu singh";
     s1.roll_no=03;
-    cout<<s1.name<<" "<<s1.roll_no<<endl;
+    // cout<<s1.name<<" "<<s1.roll_no<<endl;
     // also we can access the element of the structure with the help of pointer 
     struct student *s;
     s=&s1;
     s->name="rahul singh";
-    cout<<s->name;
+    // cout<<s->name;
+    callingchar();
 
 }
