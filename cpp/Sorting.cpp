9e99c7e30409c5b0fd21fb573dc2c68def0a4183
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-10-07 18:54:34 +0530

    binary

---
+++ cpp/Sorting.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ void bubblesort(int arr[],int n)
+ {  
+    // best case
+    // average case
+    // worst case
+ }
+ void insertionsort(int arr[],int n)
+ {
+    // best case
+    // average case
+    // worst case
+ }
+  void selectionsort(int arr[],int n) 
+ {
+    // best case o(n2)
+    // average case
+    // worst case
+    
+  }
+int main()
+{
+    int arr[10]={50,40,1,2,7,8,5,3,80,20};
+    int n=10;
+    bubblesort(arr,n);
+    insertionsort(arr,n);
+    selectionsort(arr,n);
+}
