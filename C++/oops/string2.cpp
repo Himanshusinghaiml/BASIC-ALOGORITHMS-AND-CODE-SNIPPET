55ba677692eb1cf50e7b173fc6825f10ceecb786
Himanshusinghaiml <himanshusingh945443@gmail.com>
2023-12-13 21:03:14 +0530

    add another repo

---
+++ C++/oops/string2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    string s;
+    cout<<" enter the name of the student "<<endl;
+    getline(cin,s); // getline method is used to take whole string include spaces 
+    cout<<" your name is :"<<s;
+
+}
