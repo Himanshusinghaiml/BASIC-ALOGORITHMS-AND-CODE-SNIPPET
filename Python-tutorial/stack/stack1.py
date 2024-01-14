2aa18dd862044694f52044fec6cbad5724ac05a6
Himanshusinghaiml <himanshusingh945443@gmail.com>
2024-01-14 17:14:18 +0530

    push it

---
+++ Python-tutorial/stack/stack1.py
@@ -0,0 +1,32 @@
+class stack:
+    def __init__(self) -> None:
+         self.list=[]
+         
+    def push(self,value):
+        self.list.append(value)
+        
+    def popstack(self):
+        if len(self.list)>0:
+           self.list.pop()
+        else:
+            print("statck is empy ")
+            
+    def print_stack(self):
+        for item in reversed(self.list):
+            print(item,end=" ")
+
+        
+s=stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.push(40)
+s.push(50)
+print(s.list)
+s.popstack()
+s.print_stack()
+# print(s.list)
+ 
+
+
+
