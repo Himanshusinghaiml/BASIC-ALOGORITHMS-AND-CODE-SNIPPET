f05d5347c79958282cde3756acd511176feb802b
Himanshusinghaiml <himanshusingh945443@gmail.com>
2024-01-27 13:34:35 +0530

    prime number program

--- Python-tutorial/stack/stack2.py
+++ Python-tutorial/stack/stack2.py
@@ -1,1 +1,31 @@
-print("himanshu singh ")
+# program for prime number
+import math
+def prime():
+    check=False
+    num=int(input("enter the number "))
+    if num==1 or num<1:
+        print(num,"not a prime number ")
+    else:
+        for i in range(2,int(math.sqrt(num)+2)):
+            if num%i==0:
+                check=True
+                break
+        if check==True:
+            print(num, " is not prime number ")
+        else:
+            print(num,"prime number ")
+
+def prime_recursion(num,i=2):
+    if num<2:
+        return False
+    if num%i==0:
+        return False
+    return prime_recursion(num,i+1)
+  
+num=int(input("enter the number "))
+ans=prime_recursion(num,)
+if ans==True:
+    print(" yes prime number")
+else:
+    print("not prime number")
+ 
