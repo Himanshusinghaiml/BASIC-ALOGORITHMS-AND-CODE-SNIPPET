743fd2a87acc34e1fa7564bef3fce52ca7e6e6d7
Himanshusinghaiml <himanshusingh945443@gmail.com>
2024-02-20 13:38:45 +0530

    python code

---
+++ Python-tutorial/stack/twitter.py
@@ -0,0 +1,3 @@
+def check(x,y=10,z=5):
+    print(x+y+z)
+check(x=1,y=2)
---
+++ to_find_substring.py
@@ -0,0 +1,12 @@
+name="himanshu   singh"
+if "singh" in name:
+    print("yes found ")
+else:
+    print("not found")
+print()
+print("****************************\t")
+found=name.find("singh")
+if found!=-1:
+    print("yes found")
+else:
+    print("not found")
